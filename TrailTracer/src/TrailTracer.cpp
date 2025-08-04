6e30eedc5b599b1446561d3d316d00e478cbc5f9
Nicole DeVoe Rogers <nicoledevoerogers@MacBook-Air.admin.ad.cnm.edu>
2025-08-04 14:09:36 -0600

    TrailTracer gps and speed restored and TrailTracer_Accel created and reading collision reading with date and time stamp

--- TrailTracer/src/TrailTracer.cpp
+++ TrailTracer/src/TrailTracer.cpp
@@ -9,15 +9,15 @@
 
 #include "Particle.h"
 #include <Adafruit_GPS.h>
-#include <Adafruit_MQTT.h>
-#include "Adafruit_MQTT/Adafruit_MQTT_SPARK.h"
-#include "Adafruit_MQTT/Adafruit_MQTT.h"
+// #include <Adafruit_MQTT.h>
+// #include "Adafruit_MQTT_SPARK.h"
+//#include "Adafruit_MQTT/Adafruit_MQTT.h"
 #include "Adafruit_SSD1306.h"
 #include "Adafruit_GFX.h"
-#include "GPS_CNM.h"
-#include "credentials.h"
+//#include "GPS_CNM.h"
+// #include "credentials.h"
 
-TCPClient TheClient;
+//  TCPClient TheClient;
 
 // Constants
 const int TIMEZONE = -6;
@@ -33,47 +33,47 @@ int sat;
 unsigned int lastGPS;
 
 Adafruit_GPS GPS(&Wire);
-Adafruit_MQTT_SPARK mqtt(&TheClient, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+// Adafruit_MQTT_SPARK mqtt(&TheClient, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
 Adafruit_SSD1306 display (OLED);
 
 //feeds to publish
-Adafruit_MQTT_Publish TrailGPS = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/trailtracergps");
-Adafruit_MQTT_Publish TrailSpeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/trailtracerspeed");
+//  Adafruit_MQTT_Publish TrailGPS = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/trailtracergps");
+//  Adafruit_MQTT_Publish TrailSpeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/trailtracerspeed");
 void getGPS(float *latitude, float *longitude, float *altitude, int *satellites, float *speed);
 
 
-struct TrailGPS {
-  float trailLat;
-  float trailLon;
-};
 
-float location;
-unsigned int currentTime;
-unsigned int lastTime;
-
-
-void MQTT_connect();
-bool MQTT_ping();
 
 SYSTEM_MODE(SEMI_AUTOMATIC);
-TrailGPS jackLoc;
 
-void createGPSCoordinates(TrailGPS location);
+//  struct GeoLocation {
+//    float trailLat;
+//    float trailLot;
+//  };
 
+
+// float location;
+// unsigned int currentTime;
+// unsigned int lastTime;
+//  GeoLocation jackLoc;
+
+//  void createGPSCoordinates(GeoLocation location);
+
+//  void MQTT_connect();
+//  bool MQTT_ping();
 
 void setup() {
 
-  
-Serial.begin(9600);
-waitFor(Serial.isConnected,5000);
+  Serial.begin(9600);
+  waitFor(Serial.isConnected,5000);
 
 
 
-// intialize wifi
-WiFi.on();
-WiFi.connect();
-while(WiFi.connecting()) {
-Serial.printf(".");}
+// // intialize wifi
+//   WiFi.on();
+//   WiFi.connect();
+//   while(WiFi.connecting()) {
+//   Serial.printf(".");}
 
 // intialize GPS
 GPS.begin (0x10);
@@ -95,12 +95,13 @@ delay(1000);
 
 void loop() {
 
-  MQTT_connect();
-  MQTT_ping();
+  // MQTT_connect();
+  // MQTT_ping();
 
 
   display.clearDisplay();
 
+  
   //data from GPS unit (do this continuously)
   GPS.read();
   if (GPS.newNMEAreceived()) {
@@ -119,33 +120,66 @@ void loop() {
     display.display();
   }
 
-  currentTime = millis ();
-if ((currentTime - lastTime)>10000){
-  lastTime = millis();
-  
-  jackLoc.trailLan = (34949516, 35217643)*0.000001;
-  jackLoc.trailLon = (-106754126, -106718868)*0.000001;
-  createGPSCoordinates(jackLoc);
-  
+//   currentTime = millis ();
+// if ((currentTime - lastTime)>10000) {
+//   lastTime = millis();
 }
 
-void getGPS(float *latitude, float *longitude, float *altitude, int *satellites, float *speed){
-  int theHour;
 
-  theHour = GPS.hour + TIMEZONE;
-  if(theHour < 0) {
-    theHour = theHour + 24;
-  }
+
+void getGPS(float *latitude, float *longitude, float *altitude, int *satellites, float *speed) {
+int theHour;
+
+theHour = GPS.hour + TIMEZONE;
+if(theHour < 0) {
+  theHour = theHour + 24;
+}
     
-  Serial.printf("Time: %02i:%02i:%02i:%03i\n",theHour, GPS.minute, GPS.seconds, GPS.milliseconds);
-  Serial.printf("Dates: %02i-%02i-20%02i\n", GPS.month, GPS.day, GPS.year);
-  Serial.printf("Fix: %i, Quality: %i",(int)GPS.fix,(int)GPS.fixquality);
-    if (GPS.fix) {
-      *latitude = GPS.latitudeDegrees;
-      *longitude = GPS.longitudeDegrees; 
-      *altitude = GPS.altitude;
-      *satellites = (int)GPS.satellites;
-      *speed = GPS.speed;
+Serial.printf("Time: %02i:%02i:%02i:%03i\n",theHour, GPS.minute, GPS.seconds, GPS.milliseconds);
+Serial.printf("Dates: %02i-%02i-20%02i\n", GPS.month, GPS.day, GPS.year);
+Serial.printf("Fix: %i, Quality: %i",(int)GPS.fix,(int)GPS.fixquality);
+if (GPS.fix) {
+  *latitude = GPS.latitudeDegrees;
+  *longitude = GPS.longitudeDegrees; 
+  *altitude = GPS.altitude;
+  *satellites = (int)GPS.satellites;
+  *speed = GPS.speed;
 
-    }
+  }
+
 }
+
+// void MQTT_connect() {
+//   int8_t ret;
+ 
+//   // Return if already connected.
+//   if (mqtt.connected()) {
+//     return;
+//   }
+ 
+//   Serial.print("Connecting to MQTT... ");
+ 
+//   while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+//        Serial.printf("Error Code %s\n",mqtt.connectErrorString(ret));
+//        Serial.printf("Retrying MQTT connection in 5 seconds...\n");
+//        mqtt.disconnect();
+//        delay(5000);  // wait 5 seconds and try again
+//   }
+//   Serial.printf("MQTT Connected!\n");
+// }
+
+// bool MQTT_ping() {
+//   static unsigned int last;
+//   bool pingStatus;
+
+//   if ((millis()-last)>120000) {
+//       Serial.printf("Pinging MQTT \n");
+//       pingStatus = mqtt.ping();
+//       if(!pingStatus) {
+//         Serial.printf("Disconnecting \n");
+//         mqtt.disconnect();
+//       }
+//       last = millis();
+//   }
+//   return pingStatus;
+// }
---
+++ TrailTracer_Accel/src/TrailTracer_Accel.cpp
@@ -0,0 +1,109 @@
+/* 
+ * Project: TrailTracer_Accel
+ * Author: Nicole DeVoe Rogers
+ * Date: 4 August 2025
+ * For comprehensive documentation and examples, please visit:
+ * https://docs.particle.io/firmware/best-practices/firmware-template/
+ */
+
+
+#include "Particle.h"
+#include "Wire.h"
+#include "Adafruit_SSD1306.h"
+#include "Adafruit_GFX.h"
+
+const float ACCEL_SCALE_FACTOR = 16384.0; // Default for ±2g range
+const float GRAVITY = 9.80665;            // Standard gravity in m/s^2
+const int OLED_RESET = -1;
+const int SAMPLE_PERIOD_MS = 10;               // Sample period in milliseconds
+const int SAMPLE_DURATION_S = 5;               // Sample duration in seconds
+const int ARRAY_SIZE = SAMPLE_DURATION_S * 50; // Number of samples in array
+
+String dateTime, timeOnly;
+unsigned int lastTime;
+
+Adafruit_SSD1306 display(OLED_RESET);
+float accelArray[ARRAY_SIZE];
+int arrayIndex = 0;
+
+SYSTEM_MODE(AUTOMATIC); //automatic connects to particle cloud
+
+
+void setup() {
+
+  Serial.begin(9600);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  Wire.begin();
+  // Initialize MPU6050
+  Wire.beginTransmission(0x68);
+  Wire.write(0x6B); // Power management register
+  Wire.write(0x00); // Wake up device
+  Wire.endTransmission(true);
+  // set time zone
+  Time.zone(-6);
+  Particle.syncTime(); //sync time with particle cloud
+  // Set accelerometer range to ±2g
+  Wire.beginTransmission(0x68);
+  Wire.write(0x1C); // Accelerometer configuration register
+  Wire.write(0x00); // ±2g range
+  Wire.endTransmission(true);
+
+}
+
+
+void loop() {
+
+  dateTime = Time.timeStr(); //current date and time from particle cloud
+  timeOnly = dateTime.substring(11,19); //extract the time from dateTime
+  if (millis() - lastTime > 100000) {
+    lastTime = millis();
+  }
+  
+  Time.now();
+  Wire.beginTransmission(0x68);
+  Wire.write(0x3B); // Start reading from ACCEL_XOUT_H
+  Wire.endTransmission(false);
+  Wire.requestFrom(0x68, 6, true);
+  if (Wire.available() == 6)
+  {
+    float accel_x = ((Wire.read() << 8 | Wire.read()) / ACCEL_SCALE_FACTOR) * GRAVITY;
+    float accel_y = ((Wire.read() << 8 | Wire.read()) / ACCEL_SCALE_FACTOR) * GRAVITY;
+    float accel_z = ((Wire.read() << 8 | Wire.read()) / ACCEL_SCALE_FACTOR) * GRAVITY;
+    // Store acceleration sum in array
+    accelArray[arrayIndex] = sqrt((accel_x * accel_x) + (accel_y * accel_y) + (accel_z * accel_z));
+    // Update array index
+    arrayIndex++;
+    if (arrayIndex >= ARRAY_SIZE)
+    {
+      arrayIndex = 0; // Reset index for next cycle
+    }
+    // Find maximum value in array
+    float maxAccel = accelArray[0];
+    for (int i = 1; i < ARRAY_SIZE; i++)
+    {
+      if (maxAccel < accelArray[i])
+      {
+        maxAccel = accelArray[i];
+      }
+    }
+    // Print maximum acceleration
+    Serial.printf("Max acceleration: %.2f g\n", maxAccel / GRAVITY);
+    // Display maximum acceleration on OLED screen
+    display.clearDisplay();
+    display.setTextColor(WHITE);
+    display.setTextSize(1);
+    display.setCursor(0, 0);
+    //display.print(Time.format("%A , %B %d"));
+    display.printf("Date and Time is %s \n", dateTime.c_str());
+    // display.setCursor(0, 20);
+    // display.print(Time.format("%I : %M : %S %p"));
+    display.setCursor(0, 40);
+    display.printf("Max Accel: %.2f g", maxAccel / GRAVITY);
+    display.display();
+  }
+  else
+  {
+    Serial.println("Error reading from MPU6050");
+  }
+  delay(SAMPLE_PERIOD_MS);
+}
